45232fad18201be9b813fe08b012536f1c25aef9
Ankita Samal <ankitasamal102@gmail.com>
2026-02-11 15:09:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const title = document.getElementById("title");
 const msg = document.getElementById("msg");
 const gif = document.getElementById("gif");
 
+// apna Google Form prefill link yahan daalo
+const formBaseURL = "https://docs.google.com/forms/d/e/1FAIpQLSc3GIwb0G2DjjvJgUM8mNkbIJAsZTM0f_e5agrscpauuJmJBw/viewform?usp=pp_url&entry.2089083960=ANKITA&entry.1023733850=";
+
 /* floating hearts */
 setInterval(()=>{
   const heart = document.createElement("div");
@@ -15,71 +18,39 @@ setInterval(()=>{
   setTimeout(()=>heart.remove(),6000);
 },500);
 
-noBtn.addEventListener("click", noClicked);
-yesBtn.addEventListener("click", yesClicked);
+noBtn.addEventListener("click", ()=>{
+  saveAnswer("NO");
 
-function noClicked(){
   music.play();
-  let x = Math.random() * 200;
-  let y = Math.random() * 200;
-  noBtn.style.left = x + "px";
-  noBtn.style.top = y + "px";
-}
-function noClicked(){
-  music.play();
-
-  // sad gif
-  gif.src = "sad.gif";   // apni sad gif ka naam yahan do
-
-  // sad message
+  gif.src = "sad.gif";
   title.innerHTML = "Oh no… 😢";
   msg.innerHTML = `
     Dil thoda sa toot gaya 💔<br>
-    Par phir bhi tumhari khushi meri priority hai…<br>
-    Main wait kar sakti hoon 🥺
+    Par phir bhi tumhari khushi meri priority hai…
   `;
 
-  // move NO button
   let x = Math.random() * 200;
   let y = Math.random() * 200;
   noBtn.style.left = x + "px";
   noBtn.style.top = y + "px";
-}
+});
 
+yesBtn.addEventListener("click", ()=>{
+  saveAnswer("YES");
 
-function yesClicked(){
   title.innerHTML = "Yayyyy 🥰💖";
   msg.innerHTML = `
     I knew you would say YES 😘<br>
-    Tumhare saath har din special lagta hai…<br>
-  Aur aaj se tum sirf Valentine nahi,<br>
-  meri duniya ho 💕💫<br><br>
-  I love you 😘💌<br><br>
-    
+    Tum meri duniya ho 💕
   `;
-  
-  gif.src = "iilu.gif";  
+  gif.src = "iilu.gif";
 
   yesBtn.style.display="none";
   noBtn.style.display="none";
+});
+
+// Google form save function
+function saveAnswer(ans){
+  let url = formBaseURL + ans;
+  fetch(url, { mode: "no-cors" });
 }
-window.onload = function(){
-  let name = prompt("Apna naam likho ❤️");
-  // let ans = confirm("Kya tum mujhe pasand karte ho?");
-
-  let url = "https://docs.google.com/forms/d/e/1FAIpQLSc3GIwb0G2DjjvJgUM8mNkbIJAsZTM0f_e5agrscpauuJmJBw/viewform?usp=pp_url&entry.2089083960=ANKITA&entry.1023733850=yes";
-
-  if(ans){
-    url = url.replace("test", name).replace("YES", "YES");
-  } else {
-    url = url.replace("test", name).replace("YES", "NO");
-  }
-
-  fetch(url, {mode:"no-cors"});
-}
-
-
- 
-
-
-
